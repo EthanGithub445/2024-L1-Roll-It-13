cbec9e24d5790b1cdcd9cb97b6135b3390489da1
EthanGithub445 <159392290+EthanGithub445@users.noreply.github.com>
2024-03-01 10:52:25 +1300

    Add files via upload

---
+++ 05_Rounds_v1.py
@@ -0,0 +1,75 @@
+import random
+
+
+# generates an integer between 0 6
+# to simulate roll dice
+
+def roll_die():
+    result = random.randint(1, 6)
+    return  result
+
+def two_rolls():
+    double_score = "no"
+
+    # roll two dice
+    roll_1 = roll_die()
+    roll_2 = roll_die()
+
+    # check if we have a double score opportunity
+    if roll_1 == roll_2:
+        double_score = "yes"
+
+    # find the total points so far
+    user_points = roll_1 + roll_2
+
+    # show user the result
+    print(f"Die 1: {roll_1} \t Die 2: {roll_2}")
+
+    return user_points, double_score
+
+# main routine starts here
+print("please press <enter> to begin this round: ")
+input()
+
+# get initial dice roll for user
+user_first = two_rolls()
+user_points = user_first[0]
+double_points = user_first[1]
+
+
+# tell the user if they are eligible for double points
+if double_points == "no":
+    double_feedback = ""
+else:
+    double_feedback = "if you win this round, you gain double points!"
+
+# output initial move results
+print(f"you rolled a total of {user_points}. {double_feedback}")
+print()
+
+# get initial dice rolls for computer
+computer_first = two_rolls()
+computer_points = computer_first[0]
+
+print(f"The computer rolled a total of {computer_points}.")
+
+# Loop (while both user / computer have <= 13 points)...
+while computer_points <=13 and user_points <= 13:
+
+    # ask user if they want to roll again, update
+    # points / status
+    print()
+    roll_again = input("do you want to roll the dice (type no to pass): ")
+    if roll_again == "yes":
+        user_move = roll_die()
+        user_points += user_move
+        print(f"you rolled a {user_move}. you now have {user_points} points.")
+
+    print("\nPress <enter> to continue...")
+    input()
+
+    # roll die for computer and update computer points
+    computer_move = roll_die()
+    computer_points += computer_move
+    print(f"the computer rolled a {computer_move}. The computer"
+          f" now has {computer_points}.")
